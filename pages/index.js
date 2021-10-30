61ba00061f8291f37842256f2c843a98cd135ce8
abdul waheed <a.waheed3738@gmail.com>
2021-10-30 12:38:33 +0500

    added card Shuffling logic and reset game logic

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import {useState} from "react";
+import {Body} from "next/dist/server/web/spec-compliant/body";
 
-const image = [
+const cardImages = [
     {'src' : '/img/helmet-1.png'},
     {'src' : '/img/potion-1.png'},
     {'src' : '/img/ring-1.png'},
@@ -11,9 +13,19 @@ const image = [
 
 
 export default function Home() {
+    const [cards,setCards] = useState([])
+    const [turns,setTurns] = useState(0)
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages,...cardImages].sort(() => Math.random() - 0.5)
+            .map((image) => ({...image,id : Math.random()}))
+        setCards(shuffledCards)
+        setTurns(0)
+    }
   return (
-    <div className=''>
-    <h1 className={'bg-indigo-900'}>hhahaha</h1>
+    <div className='text-center'>
+        <h1 className={' text-2xl font-sans font-bold'}>Memory game</h1>
+        <button onClick={shuffleCards} className={'bg-green-500 px-3 mt-6 rounded-2xl text-white py-3 shadow-2xl hover:bg-green-700'}>New Game</button>
+
     </div>
   )
 }
