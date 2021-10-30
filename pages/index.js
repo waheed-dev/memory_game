b555c4e567d4712dc50ba69ce8246c4d677718ca
abdul waheed <a.waheed3738@gmail.com>
2021-10-30 22:26:25 +0500

    added Grid and some minor sryling

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,21 @@ export default function Home() {
         setTurns(0)
     }
   return (
-    <div className='text-center'>
+    <div className='text-center mx-auto'>
         <h1 className={' text-2xl font-sans font-bold'}>Memory game</h1>
         <button onClick={shuffleCards} className={'bg-green-500 px-3 mt-6 rounded-2xl text-white py-3 shadow-2xl hover:bg-green-700'}>New Game</button>
+        <div>
+        </div>
+        <div className={'grid grid-cols-4 gap-4 mt-4 w-[50%] mx-auto'}>
+
+        {cards.map(card => (<div key={card.id} className={''}>
+            <div className={''}><img className={'h-[150px] mb-1'} src={card.src} alt={'photo front side'}/></div>
+            <div><img src={'./img/cover.png'} className={'h-[150px]'} alt={'photo back side'}/>
+            </div>
+        </div>
+
+        ))}
+        </div>
 
     </div>
   )
